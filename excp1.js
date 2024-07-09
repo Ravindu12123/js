acc6eb99200e9fa0e9183513387ad77faf3aec47
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-07-09 15:10:15 +0530

    Update and rename excp.js to excp1.js

--- excp1.js
+++ excp1.js
@@ -1,16 +1,18 @@
 async function run() {
     var getUrl = window.location;
     var ho = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-    alert(ho);
+    console.log(ho);
     tgg=document.cookie.split(";");
     tgf=tgg.find(er=>er.includes("tp"));
     tgp=tgf.split("=")[1];
     d3=`newwpsafelink4=${tgp}`;
+    console.log(tgp,d3);
+    console.log('fetching!!!!!');
     x=await fetch(ho,{
         method:'POST',
         headers:{
          'Content-Type': 'application/x-www-form-urlencoded',
-         Cookie:document.cookie
+         'Cookie':document.cookie
         },
         body:d3
     }).then(xl=>xl.text());
