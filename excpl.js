c0dd1fc738ca6e3e7031f833f139ac82dea3de83
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-07-09 15:00:25 +0530

    Create excpl.js

---
+++ excpl.js
@@ -0,0 +1,22 @@
+async function run() {
+    var getUrl = window.location;
+    var ho = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+    alert(ho);
+    tgg=document.cookie.split(";");
+    tgf=tgg.find(er=>er.includes("tp"));
+    tgp=tgf.split("=")[1];
+    d3=`newwpsafelink4=${tg}`;
+    x=await fetch(ho,{
+        method:'POST',
+        headers:{
+         'Content-Type': 'application/x-www-form-urlencoded',
+         Cookie:document.cookie
+        },
+        body:d3
+    }).then(xl=>xl.text());
+    ls=/\/(.*?)"/g.exec(x);
+    ls= ls==null ? null:ls[1];
+    console.log(`Url Found!: ${ls}`);
+    ul=ho+ls;
+    console.log(ul);
+          }
